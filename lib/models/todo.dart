e5bbdfcf0bb31db5ccc972af3a5679d9064bd795
João Bruno <jbrun0rjbrun0r@gmail.com>
2022-06-01 18:59:14 -0300

    cmmt full projeto
    upando projeto app lista de tarefas - flutter: web/android

---
+++ lib/models/todo.dart
@@ -0,0 +1,19 @@
+class Todo {
+
+  Todo({required this.title, required this.dateTime});
+
+  Todo.fromJson(Map<String, dynamic> json)
+    : title = json['title'],
+      dateTime = DateTime.parse(json['datetime']);
+
+  String title;
+  DateTime dateTime;
+
+  toJson(){
+    return {
+      'title': title,
+      'datetime': dateTime.toIso8601String(),
+    };
+  }
+
+}
